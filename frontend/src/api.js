9f838202d1a980e2f4a4be07299423c58784a94d
diegohernandez8345-glitch <diegohernandez8345@gmail.com>
2025-12-07 17:54:07 -0600

    Un error en la función eliminar

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,19 +1,31 @@
 const API_BASE = "https://agenda-app-4iqk.onrender.com/api";
 
+// Obtener tareas
 export async function getTareas() {
   const res = await fetch(`${API_BASE}/tareas`);
   return res.json();
 }
 
+// Crear tarea
 export async function crearTarea(data) {
   const res = await fetch(`${API_BASE}/tareas`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   });
 
   return res.json();
 }
+
+// Eliminar tarea
+export async function eliminarTarea(id) {
+  const res = await fetch(`${API_BASE}/tareas/${id}`, {
+    method: "DELETE",
+  });
+
+  return res.json();
+}
+
 
