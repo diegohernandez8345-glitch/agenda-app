939b4c71a58c54a4c752cba420181392db959239
diegohernandez8345-glitch <diegohernandez8345@gmail.com>
2025-12-07 17:31:13 -0600

    Corrección del apibase

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,19 @@
-//const API_BASE = process.env.REACT_APP_API_URL || 'https://agenda-app-4iqk.onrender.com';
 const API_BASE = "https://agenda-app-4iqk.onrender.com/api";
 
-// Obtener tareas
 export async function getTareas() {
   const res = await fetch(`${API_BASE}/tareas`);
   return res.json();
 }
 
-// Crear tarea
 export async function crearTarea(data) {
   const res = await fetch(`${API_BASE}/tareas`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(data),
   });
 
   return res.json();
 }
+
